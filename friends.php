0521c55212b5bf9fe67977adc49f22a8eafae03c
zcampaner <zcampaner@gmail.com>
2016-05-30 15:14:38 +0800

    initial commit staging for facebook api login and get friends

---
+++ friends.php
@@ -0,0 +1,15 @@
+<?php
+
+        $user_profile = $facebook->api('/me');
+        $friends = $facebook->api('/me/friends');
+
+        echo '<ul>';
+        foreach ($friends["data"] as $value) {
+            echo '<li>';
+            echo '<div class="pic">';
+            echo '<img src="https://graph.facebook.com/' . $value["id"] . '/picture"/>';
+            echo '</div>';
+            echo '<div class="picName">'.$value["name"].'</div>'; 
+            echo '</li>';
+        }
+        echo '</ul>';
